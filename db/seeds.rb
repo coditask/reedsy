8a9ea7a81ce1198ec95b02a143a0148d824fbed7
RAVI KUMAR <ravi@yogaeasy.de>
2022-01-08 19:34:40 +0100

    Seed the data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+store = Store.create name: 'Store 1'
+store2 = Store.create name: 'Store 2'
+Item.create([ { code: 'MUG', name: 'Reedsy Mug', price: 600, store_id: store.id },
+	          { code: 'MUG', name: 'Reedsy Mug', price: 600, store_id: store2.id },
+	          { code: 'TSHIRT', name: 'Reedsy T-shirt', price: 1500, store_id: store.id },
+	          { code: 'HOODIE', name: 'Reedsy Hoodie', price: 2000, store_id: store2.id }])
